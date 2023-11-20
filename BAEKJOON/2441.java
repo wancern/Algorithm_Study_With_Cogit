4a7cfc5d7118aa6f60ad1fb6f21da74cddb831f9
Hyeongu Lee <57997390+wancern@users.noreply.github.com>
2023-11-20 10:03:56 +0900

    BAEKJOON2441

---
+++ BAEKJOON/2441.java
@@ -0,0 +1,19 @@
+import java.io.*;
+​
+public class Main {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = N; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
